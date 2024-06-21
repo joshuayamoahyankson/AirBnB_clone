d5f591cc7e198ed9493609360ce4f70fe6b20a26
joshuayamoahyankson <ziriks360@gmail.com>
2024-06-21 06:40:23 +0000

    Corrections made

--- models/base_model.py
+++ models/base_model.py
@@ -7,19 +7,20 @@ import uuid
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
-        if kwargs is not None and kwargs:
+        if kwargs:
+            if "__class__" in kwargs:
+                del kwargs["__class__"]
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key in ['created_at', 'updated_at']:
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, value)
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now
-        if not kwargs:
-            storage.new(self)
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(kwargs[key]))
+                else:
+                    setattr(self, key, kwargs[key])
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now
+            if not kwargs:
+                storage.new(self)
 
     def __str__(self):
         return (f"{self.__class__.__name__}, {self.id}, {self.__dict__}")
@@ -29,12 +30,8 @@ class BaseModel:
         storage.save()
 
     def to_dict(self):
-        emp_dict = dict()
-        for key, value in self.__dict__.items():
-            if value is not None:
-                if key in ['created_at', 'updated_at']:
-                    emp_dict[key] = value.isoformat()
-                else:
-                    emp_dict[key] = value
-        emp_dict['__class__'] = self.__class__.__name__
-        return emp_dict
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = BaseModel.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
